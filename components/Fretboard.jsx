15d5bb91bf6bd0881a01c5dc0ca07c267727ebda
Luiz <louis_worklad24@outlook.com>
2025-01-28 00:01:13 -0300

    Create the search list component.

--- components/Fretboard.jsx
+++ components/Fretboard.jsx
@@ -4,15 +4,27 @@ import styles from "./css/Fretboard.module.css";
 import { getScale } from "@/lib/scales";
 
 export default function Fretboard({ notes }) {
-  const scale = getScale("C", "major")
+  const scale = getScale("C", "major");
+  const names = scale.map((note) => note.name[0]);
 
   return (
     <main className={styles.page}>
-        <div className={styles.fretboard}>
+      <div className={styles.fretboard}>
         {notes.map((note, index) => {
+          const name = note.name[0];
+          const inScale = names.find((n) => n === name);
+
           return (
-            <div key={index} className={styles.note} onClick={() => console.log(note.name[0])}>
-              {note.name[0]}
+            <div
+              key={index}
+              className={
+                inScale
+                  ? `${styles.note}`
+                  : `${styles.note} ${styles.fadednote}`
+              }
+              onClick={() => console.log(name)}
+            >
+              {name}
             </div>
           );
         })}
---
+++ components/SearchList.jsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { useState } from "react";
+import { getScaleNames } from "@/lib/scales";
+import styles from "./css/SearchList.module.css";
+
+export default function SearchList() {
+  const [query, setQuery] = useState("");
+  const [results, setResults] = useState([]);
+  const data = getScaleNames();
+
+  function handleInputChange(event) {
+    const value = event.target.value;
+
+    if (value === "") {
+      setResults([]);
+      setQuery(value);
+      return;
+    }
+
+    const filteredResults = [];
+    let count = 0;
+
+    data.forEach((element) => {
+      if (count < 5 && element.toLowerCase().startsWith(value.toLowerCase())) {
+        filteredResults.push(element);
+        count += 1;
+      }
+    });
+
+    setQuery(value);
+    setResults(filteredResults);
+    console.log(results);
+  }
+
+  function handleItemClick(item) {
+    setQuery(item);
+    setResults([]);
+  }
+
+  return (
+    <div className={`${styles.searchlist}`}>
+      <input
+        type="text"
+        value={query}
+        onChange={handleInputChange}
+        placeholder="Scale..."
+      />
+      <ul>
+        {results.map((item, index) => {
+          return (
+            <li key={index} onClick={() => handleItemClick(item)}>
+              <span>{item}</span>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
